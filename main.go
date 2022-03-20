36cdf0e996149da6a59ca88726b9c6cadaf0a3b8
miyasereina <1448472422@qq.com>
2022-03-20 21:34:36 +0800

    feat:huffman coding

--- main.go
+++ main.go
@@ -3,14 +3,22 @@ package main
 import "struct/binaryTree"
 
 func main() {
-	root := binaryTree.BiTNode{}
-	left := binaryTree.BiTNode{}
-	right := binaryTree.BiTNode{}
-	root.Data = 1
-	left.Data = 2
-	right.Data = 3
-	root.Lchild = &left
-	root.Rchild = &right
-	root.Preorder(&root)
+	//root := binaryTree.BiTNode{}
+	//left := binaryTree.BiTNode{}
+	//right := binaryTree.BiTNode{}
+	//root.Data = 1
+	//left.Data = 2
+	//right.Data = 3
+	//root.Lchild = &left
+	//root.Rchild = &right
+	//root.Preorder(&root)
+	var a, b, c binaryTree.HuTNode
+	a.Weight = 1
+	a.Value = "a"
+	b.Weight = 2
+	b.Value = "b"
+	c.Value = "c"
+	c.Weight = 3
+	binaryTree.HuffmanEncode([]binaryTree.HuTNode{a, b, c})
 
 }
